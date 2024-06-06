8d008c601daf725a33d607a21de80907a6af1391
Dmitri Rusnac <dmitri.rusnac@iongroup.com>
2024-06-06 12:46:48 +0300

    Update tests display names

--- src/test/java/com/cedacri/resource/MoviesResourceTest.java
+++ src/test/java/com/cedacri/resource/MoviesResourceTest.java
@@ -7,6 +7,7 @@ import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.MediaType;
 import org.jboss.logging.annotations.Message;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -39,21 +40,21 @@ class MoviesResourceTest {
     );
 
     @Test
-    @Message("serviciul extern intoarece acelasi raspuns pentru orice chemare")
+    @DisplayName("serviciul extern intoarece acelasi raspuns pentru orice chemare")
     public void getMovies() {
         assertEquals(externalService.getMovies(), movies);
         assertEquals(externalService.getMovies(), movies);
     }
 
     @Test
-    @Message("serviciul extern intoarce raspusnuri in functie de valoarea la parametri")
+    @DisplayName("serviciul extern intoarce raspusnuri in functie de valoarea la parametri")
     public void getMovieById() {
         assertEquals("Titanic", externalService.getMovieById(0L).getTitle());
         assertEquals("The Matrix", externalService.getMovieById(1L).getTitle());
     }
 
     @Test
-    @Message("serviciul extern intoarce raspusnuri in functie de valoarea la parametri")
+    @DisplayName("serviciul extern intoarce raspusnuri in functie de valoarea la parametri")
     public void getBestMovies() {
         assertEquals(8, bestMoviesService.findBestMovies(6.0).size());
         assertEquals(6, bestMoviesService.findBestMovies(8.0).size());
@@ -61,7 +62,7 @@ class MoviesResourceTest {
     }
 
     @Test
-    @Message("serviciul extern semnaleaza o careva eroare")
+    @DisplayName("serviciul extern semnaleaza o careva eroare")
     public void errorTest(){
         assertThrows(Exception.class, () -> externalService.getMovieById(999L));
     }
@@ -77,7 +78,7 @@ class MoviesResourceTest {
     }
 
     @Test
-    @Message("serviciul extern intoarce lista de valori definite in clasa SomeExternalService, cu exceptia la un test, unde se va crea un mock")
+    @DisplayName("serviciul extern intoarce lista de valori definite in clasa SomeExternalService, cu exceptia la un test, unde se va crea un mock")
     public void mockServiceTest() {
         SomeExternalService spyExternalService = spy(externalService);
 
@@ -91,7 +92,7 @@ class MoviesResourceTest {
     }
 
     @Test
-    @Message("serviciul extern intoarce raspunsuri diferite pentru chemari consecutive")
+    @DisplayName("serviciul extern intoarce raspunsuri diferite pentru chemari consecutive")
     public void consecutiveCallsTest(){
         SomeExternalService spyExternalService = spy(externalService);
 
