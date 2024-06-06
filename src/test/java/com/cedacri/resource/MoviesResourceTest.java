4e379d46d65ede389ac52c56d24f48dd49ed8c8f
Dmitri Rusnac <dmitri.rusnac@iongroup.com>
2024-06-06 12:13:52 +0300

    Update tests

--- src/test/java/com/cedacri/resource/MoviesResourceTest.java
+++ src/test/java/com/cedacri/resource/MoviesResourceTest.java
@@ -6,6 +6,7 @@ import com.cedacri.service.SomeExternalService;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.logging.annotations.Message;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -26,29 +27,41 @@ class MoviesResourceTest {
     List<Movie> movies = Arrays.asList(
             new Movie(0L, "Titanic", "James Cameron", 7.9),
             new Movie(1L, "The Matrix", "The Wachowskis", 8.7),
-            new Movie(2L, "Interstellar", "Christopher Nolan", 8.8)
+            new Movie(2L, "Interstellar", "Christopher Nolan", 8.8),
+            new Movie(3L, "Inception", "Christopher Nolan", 5.9),
+            new Movie(4L, "The Shawshank Redemption", "Frank Darabont", 9.3),
+            new Movie(5L, "Pulp Fiction", "Quentin Tarantino", 2.9),
+            new Movie(6L, "Forrest Gump", "Robert Zemeckis", 8.8),
+            new Movie(7L, "The Dark Knight", "Christopher Nolan", 9.0),
+            new Movie(8L, "Fight Club", "David Fincher", 3.8),
+            new Movie(9L, "The Godfather", "Francis Ford Coppola", 6.2),
+            new Movie(10L, "The Lion King", "Roger Allers, Rob Minkoff", 8.5)
     );
 
     @Test
+    @Message("serviciul extern intoarece acelasi raspuns pentru orice chemare")
     public void getMovies() {
         assertEquals(externalService.getMovies(), movies);
         assertEquals(externalService.getMovies(), movies);
     }
 
     @Test
+    @Message("serviciul extern intoarce raspusnuri in functie de valoarea la parametri")
     public void getMovieById() {
         assertEquals("Titanic", externalService.getMovieById(0L).getTitle());
         assertEquals("The Matrix", externalService.getMovieById(1L).getTitle());
     }
 
     @Test
+    @Message("serviciul extern intoarce raspusnuri in functie de valoarea la parametri")
     public void getBestMovies() {
-        assertEquals(3, bestMoviesService.findBestMovies(3.0).size());
-        assertEquals(2, bestMoviesService.findBestMovies(8.0).size());
-        assertEquals(1, bestMoviesService.findBestMovies(8.8).size());
+        assertEquals(8, bestMoviesService.findBestMovies(6.0).size());
+        assertEquals(6, bestMoviesService.findBestMovies(8.0).size());
+        assertEquals(2, bestMoviesService.findBestMovies(9.0).size());
     }
 
     @Test
+    @Message("serviciul extern semnaleaza o careva eroare")
     public void errorTest(){
         assertThrows(Exception.class, () -> externalService.getMovieById(999L));
     }
@@ -56,14 +69,30 @@ class MoviesResourceTest {
     @Test
     public void integrationRestTest() {
         given()
-                .when().get("/movies/best")
+                .when()
+                .get("/movies/best")
                 .then()
                 .statusCode(200)
                 .contentType(MediaType.APPLICATION_JSON);
     }
 
     @Test
+    @Message("serviciul extern intoarce lista de valori definite in clasa SomeExternalService, cu exceptia la un test, unde se va crea un mock")
     public void mockServiceTest() {
+        SomeExternalService spyExternalService = spy(externalService);
+
+        doReturn(Arrays.asList(
+                new Movie(0L, "Titanic", "James Cameron", 7.9),
+                new Movie(1L, "The Matrix", "The Wachowskis", 8.7)
+        )).when(spyExternalService).getMovies();
+
+        assertEquals(2, spyExternalService.getMovies().size());
+        assertNotNull(spyExternalService.getMovieById(5L));
+    }
+
+    @Test
+    @Message("serviciul extern intoarce raspunsuri diferite pentru chemari consecutive")
+    public void consecutiveCallsTest(){
         SomeExternalService spyExternalService = spy(externalService);
 
         doReturn(Arrays.asList(
@@ -75,8 +104,6 @@ class MoviesResourceTest {
 
         assertEquals(2, spyExternalService.getMovies().size());
         assertEquals(1, spyExternalService.getMovies().size());
-
-        assertNotNull(spyExternalService.getMovieById(2L));
     }
 
 }
